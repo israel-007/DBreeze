2538de465a785df4aaed73040f17a6fac1d12126
israel-007 <abimboluoluyemi@gmail.com>
2025-01-10 20:21:18 +0100

    DBreeze: app.env moved to the root folder, settings.php has been updated with the changes.

--- DBreeze/vendor/Settings/Settings.php
+++ DBreeze/vendor/Settings/Settings.php
@@ -3,27 +3,28 @@
 function loadEnv($filePath)
 {
     if (!file_exists($filePath)) {
-        throw new Exception("The .env file does not exist: $filePath");
-    }
+        exit("The .env file does not exist: $filePath");
+    } else {
 
-    $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) {
-            continue;
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            putenv("$name=$value");
+            $_ENV[$name] = $value;
+            $_SERVER[$name] = $value;
         }
-
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
-
-        putenv("$name=$value");
-        $_ENV[$name] = $value;
-        $_SERVER[$name] = $value;
     }
 }
 
-loadEnv(__DIR__ . '/../../config/app.env');
+loadEnv(__DIR__ . '/../../app.env');
 
 if ($_SERVER['HTTP_HOST'] == 'localhost' or $_SERVER['HTTP_HOST'] == '127.0.0.1' or $_SERVER['HTTP_HOST'] == '192.168.43.96') {
 
